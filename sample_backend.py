d1614f18394dfbf0b927f4d2baff3dd174814fbf
Trilok Debbad <tdebbad@Triloks-MacBook-Pro.local>
2021-01-13 02:12:48 -0800

    Added option to get users that match given name and given job

--- sample_backend.py
+++ sample_backend.py
@@ -42,10 +42,17 @@ def hello_world():
 def get_users():
    if request.method == 'GET':
       search_username = request.args.get('name')
-      if search_username :
+      search_job = request.args.get('job')
+      if search_username and search_job:
          subdict = {'users_list' : []}
          for user in users['users_list']:
-            if user['name'] == search_username:
+            if user['name'] == search_username and user['job'] == search_job :
+               subdict['users_list'].append(user)
+         return subdict
+      elif search_username:
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username :
                subdict['users_list'].append(user)
          return subdict
       return users
