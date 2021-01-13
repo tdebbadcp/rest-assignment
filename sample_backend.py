722bb502f542fb4f0d4ad20fb8167414dabaf5fe
Trilok Debbad <tdebbad@Triloks-MacBook-Pro.local>
2021-01-13 01:24:43 -0800

    Delete particular user from the list by ID

--- sample_backend.py
+++ sample_backend.py
@@ -57,11 +57,19 @@ def get_users():
       # 200 is the default code for a normal response
       return resp
 
-@app.route('/users/<id>')
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
-   if id :
-      for user in users['users_list']:
-        if user['id'] == id:
-           return user
-      return ({})
-   return users
+   if request.method == 'GET':
+      if id :
+         for user in users['users_list']:
+            if user['id'] == id:
+               return user
+         return ({})
+      return users
+   elif request.method == 'DELETE':
+      if id :
+         for user in users['users_list']:
+            if user['id'] == id:
+               users['users_list'].remove(user)
+               resp = jsonify(success=True)
+         return resp
