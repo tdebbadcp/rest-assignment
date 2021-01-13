77059dc6c055f5edab728c675e7fc7a9012b8096
Trilok Debbad <tdebbad@Triloks-MacBook-Pro.local>
2021-01-12 22:35:21 -0800

    fetching users by name

--- sample_backend.py
+++ sample_backend.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 users = { 
@@ -38,4 +39,11 @@ def hello_world():
 
 @app.route('/users')
 def get_users():
+   search_username = request.args.get('name') #accessing the value of parameter 'name'
+   if search_username :
+      subdict = {'users_list' : []}
+      for user in users['users_list']:
+         if user['name'] == search_username:
+            subdict['users_list'].append(user)
+      return subdict
    return users
