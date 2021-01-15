aa059e27cd932bd13e3f2175880ff184e262c679
Trilok Debbad <tdebbad@Triloks-MacBook-Pro.local>
2021-01-15 01:28:44 -0800

    Changed 200 code to 201 code for POST call

--- sample_backend.py
+++ sample_backend.py
@@ -61,7 +61,7 @@ def get_users():
    elif request.method == 'POST':
       userToAdd = request.get_json()
       users['users_list'].append(userToAdd)
-      resp = jsonify(success=True)
+      resp = jsonify(), 201
       #resp.status_code = 200 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
       return resp
